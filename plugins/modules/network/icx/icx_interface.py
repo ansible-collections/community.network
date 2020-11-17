e179537f837bef8e3d6f2dba80265886328e8632
sumit <sumit@alethea.in>
2020-11-17 13:35:17 +0530

    removed skip from icx_interface

--- plugins/modules/network/icx/icx_interface.py
+++ plugins/modules/network/icx/icx_interface.py
@@ -664,7 +664,6 @@ def main():
     result['changed'] = False
     if warnings:
         result['warnings'] = warnings
-    exec_command(module, 'skip')
     want = map_params_to_obj(module)
     have = map_config_to_obj(module)
     commands = map_obj_to_commands((want, have))

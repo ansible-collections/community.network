1d670b940447fbe8428374131ea60b984bb10d17
Ganesh B Nalawade <ganesh634@gmail.com>
2020-11-17 17:34:48 +0530

    Fixed action plugins import non-existing module from ansible.netcommon issue
    *  Remove broken import statement which is not used from within action plugins

--- plugins/action/enos.py
+++ plugins/action/enos.py
@@ -23,7 +23,6 @@ import copy
 from ansible import constants as C
 from ansible_collections.ansible.netcommon.plugins.action.network import ActionModule as ActionNetworkModule
 from ansible_collections.community.network.plugins.module_utils.network.enos.enos import enos_provider_spec
-from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.warnings import deprecate
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import load_provider
 from ansible.utils.display import Display
 

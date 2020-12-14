254c8ff5441750913cd377f57c75c3197c3c340a
sumit <sumit@alethea.in>
2020-12-14 23:25:25 +0530

    check_running_config set False

--- plugins/modules/network/icx/icx_static_route.py
+++ plugins/modules/network/icx/icx_static_route.py
@@ -62,7 +62,7 @@ options:
       check_running_config:
         description:
           - Check running configuration. This can be set as environment variable.
-           Module will use environment variable value(default:True), unless it is overridden, by specifying it as module parameter.
+           Module will use environment variable value(default:False), unless it is overridden, by specifying it as module parameter.
         type: bool
   purge:
     description:
@@ -78,9 +78,9 @@ options:
   check_running_config:
     description:
       - Check running configuration. This can be set as environment variable.
-       Module will use environment variable value(default:True), unless it is overridden, by specifying it as module parameter.
+       Module will use environment variable value(default:False), unless it is overridden, by specifying it as module parameter.
     type: bool
-    default: yes
+    default: no
 '''
 
 EXAMPLES = """
@@ -258,7 +258,7 @@ def main():
         next_hop=dict(type='str'),
         admin_distance=dict(type='int'),
         state=dict(default='present', choices=['present', 'absent']),
-        check_running_config=dict(default=True, type='bool', fallback=(env_fallback, ['ANSIBLE_CHECK_ICX_RUNNING_CONFIG']))
+        check_running_config=dict(default=False, type='bool', fallback=(env_fallback, ['ANSIBLE_CHECK_ICX_RUNNING_CONFIG']))
     )
 
     aggregate_spec = deepcopy(element_spec)

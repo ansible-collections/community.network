d0ee3d2de5c7fa6608e133028bb1bb9491116ab8
swaroop <swaroop.r@alethea.in>
2020-12-28 13:00:29 +0530

    Add icx_aaa_accounting_console.py with choices

--- plugins/modules/network/icx/icx_aaa_accounting_console.py
+++ plugins/modules/network/icx/icx_aaa_accounting_console.py
@@ -26,15 +26,19 @@ options:
 (Super User level - all commands), 4 (Port Configuration level - port-config and read-only commands), and 5 (Read Only level -
 read-only commands)
           type: int
+          choices: [0,4,5]
         primary_method:
           description: primary accounting method.
           type: string
+          choices: ['radius','tacacs+','none']
         backup_method1:
           description: backup accounting method if primary method fails.
           type: string
+          choices: ['radius','tacacs+','none']
         backup_method2:
           description: bacup accounting method if primary and backup1 metthods fail.
           type: string
+          choices: ['none']
         state:
           description: Specifies whether to configure or remove rate limiting.
           type: str
@@ -46,9 +50,11 @@ read-only commands)
         primary_method:
           description: primary accounting method.
           type: string
+          choices: ['radius','none']
         backup_method1:
           description: backup accounting method if primary method fails.
           type: string
+          choices: ['none']
         state:
           description: Specifies whether to configure or remove rate limiting.
           type: str
@@ -60,12 +66,15 @@ read-only commands)
         primary_method:
           description: primary accounting method.
           type: string
+          choices: ['radius','tacacs+','none']
         backup_method1:
           description: backup accounting method if primary method fails.
           type: string
+          choices: ['radius','tacacs+','none']
         backup_method2:
           description: bacup accounting method if primary and backup1 metthods fail.
           type: string
+          choices: ['none']
         state:
           description: Specifies whether to configure or remove rate limiting.
           type: str
@@ -77,9 +86,11 @@ read-only commands)
         primary_method:
           description: primary accounting method.
           type: string
+          choices: ['radius','none']
         backup_method1:
           description: backup accounting method if primary method fails.
           type: string
+          choices: ['none']
         state:
           description: Specifies whether to configure or remove rate limiting.
           type: str
@@ -91,12 +102,15 @@ read-only commands)
         primary_method:
           description: primary accounting method.
           type: string
+          choices: ['radius','tacacs+','none']
         backup_method1:
           description: backup accounting method if primary method fails.
           type: string
+          choices: ['radius','tacacs+','none']
         backup_method2:
           description: bacup accounting method if primary and backup1 metthods fail.
           type: string
+          choices: ['none']
         state:
           description: Specifies whether to configure or remove rate limiting.
           type: str
@@ -133,22 +147,12 @@ def build_command(module, commands=None, dot1x=None, exec_=None, mac_auth=None, 
         else:
             cmd = "aaa accounting commands {} default start-stop".format(commands['privilege_level'])      
 
-        if commands['primary_method'] in ['radius', 'tacacs+', 'none']:
+        if commands['primary_method'] is not None:
             cmd+= " {}".format(commands['primary_method'])
-            if commands['backup_method1'] in ['radius', 'tacacs+', 'none'] and commands['primary_method']!= 'none':
+            if commands['backup_method1'] is not None:
                 cmd+= " {}".format(commands['backup_method1'])
-                if commands['backup_method2'] in ['radius', 'tacacs+', 'none'] and commands['backup_method1']!= 'none':
+                if commands['backup_method2'] is not None:
                     cmd+= " {}".format(commands['backup_method2'])
-                elif commands['backup_method2'] is not None:
-                    module.fail_json(msg="Invalid input -> {}".format(commands['backup_method2']))
-            elif commands['backup_method1'] is not None:
-                module.fail_json(msg="Invalid input -> {}".format(commands['backup_method1']))
-        elif commands['primary_method'] is not None:
-            module.fail_json(msg="Invalid input -> {}".format(commands['primary_method']))
-
-        if commands['primary_method'] == commands['backup_method1'] or commands['primary_method'] == commands['backup_method2'] or commands['backup_method1'] == commands['backup_method2']:
-            if commands['backup_method1'] is not None and commands['backup_method2'] is not None:
-                module.fail_json(msg="Incomplete command.")
         command.append(cmd)
 
     if dot1x is not None:
@@ -158,17 +162,10 @@ def build_command(module, commands=None, dot1x=None, exec_=None, mac_auth=None, 
         else:
             cmd = "aaa accounting dot1x default start-stop"   
 
-        if dot1x['primary_method'] in ['radius', 'none']:
+        if dot1x['primary_method'] is not None:
             cmd+= " {}".format(dot1x['primary_method'])
-            if dot1x['backup_method1'] in ['radius', 'none'] and dot1x['primary_method']!='none':
+            if dot1x['backup_method1'] is not None:
                 cmd+= " {}".format(dot1x['backup_method1'])
-            elif dot1x['backup_method1'] is not None:
-                module.fail_json(msg="Invalid input -> {}".format(dot1x['backup_method1']))
-        elif dot1x['primary_method'] is not None:
-            module.fail_json(msg="Invalid input -> {}".format(dot1x['primary_method']))
-
-        if dot1x['primary_method'] == dot1x['backup_method1']:
-            module.fail_json(msg="Incomplete command.")
         command.append(cmd)
 
     if exec_ is not None:
@@ -178,22 +175,12 @@ def build_command(module, commands=None, dot1x=None, exec_=None, mac_auth=None, 
         else:
             cmd = "aaa accounting exec default start-stop"      
 
-        if exec_['primary_method'] in ['radius', 'tacacs+', 'none']:
+        if exec_['primary_method'] is not None:
             cmd+= " {}".format(exec_['primary_method'])
-            if exec_['backup_method1'] in ['radius', 'tacacs+', 'none'] and exec_['primary_method']!= 'none':
+            if exec_['backup_method1'] is not None:
                 cmd+= " {}".format(exec_['backup_method1'])
-                if exec_['backup_method2'] in ['radius', 'tacacs+', 'none'] and exec_['backup_method1']!= 'none':
+                if exec_['backup_method2'] is not None:
                     cmd+= " {}".format(exec_['backup_method2'])
-                elif exec_['backup_method2'] is not None:
-                    module.fail_json(msg="Invalid input -> {}".format(exec_['backup_method2']))
-            elif exec_['backup_method1'] is not None:
-                module.fail_json(msg="Invalid input -> {}".format(exec_['backup_method1']))
-        elif exec_['primary_method'] is not None:
-            module.fail_json(msg="Invalid input -> {}".format(exec_['primary_method']))
-
-        if exec_['primary_method'] == exec_['backup_method1'] or exec_['primary_method'] == exec_['backup_method2'] or exec_['backup_method1'] == exec_['backup_method2']:
-            if exec_['backup_method1'] is not None and exec_['backup_method2'] is not None:
-                module.fail_json(msg="Incomplete command.")
         command.append(cmd)
 
     if mac_auth is not None:
@@ -203,17 +190,10 @@ def build_command(module, commands=None, dot1x=None, exec_=None, mac_auth=None, 
         else:
             cmd = "aaa accounting mac-auth default start-stop"   
 
-        if mac_auth['primary_method'] in ['radius', 'none']:
+        if mac_auth['primary_method'] is not None:
             cmd+= " {}".format(mac_auth['primary_method'])
-            if mac_auth['backup_method1'] in ['radius', 'none'] and mac_auth['primary_method']!='none':
+            if mac_auth['backup_method1'] is not None:
                 cmd+= " {}".format(mac_auth['backup_method1'])  
-            elif mac_auth['backup_method1'] is not None:
-                module.fail_json(msg="Invalid input -> {}".format(mac_auth['backup_method1']))
-        elif mac_auth['primary_method'] is not None:
-            module.fail_json(msg="Invalid input -> {}".format(mac_auth['primary_method']))
-            
-        if mac_auth['primary_method'] == mac_auth['backup_method1']:
-            module.fail_json(msg="Incomplete command.")
         command.append(cmd)
 
     if system is not None:
@@ -223,22 +203,12 @@ def build_command(module, commands=None, dot1x=None, exec_=None, mac_auth=None, 
         else:
             cmd = "aaa accounting system default start-stop"      
         
-        if system['primary_method'] in ['radius', 'tacacs+', 'none']:
+        if system['primary_method'] is not None:
             cmd+= " {}".format(system['primary_method'])
-            if system['backup_method1'] in ['radius', 'tacacs+', 'none'] and system['primary_method']!='none':
+            if system['backup_method1'] is not None:
                 cmd+= " {}".format(system['backup_method1'])
-                if system['backup_method2'] in ['radius', 'tacacs+', 'none'] and system['backup_method1']!='none':
+                if system['backup_method2'] is not None:
                     cmd+= " {}".format(system['backup_method2'])  
-                elif system['backup_method2'] is not None:
-                    module.fail_json(msg="Invalid input -> {}".format(system['backup_method2']))
-            elif system['backup_method1'] is not None:
-                module.fail_json(msg="Invalid input -> {}".format(system['backup_method1']))
-        elif system['primary_method'] is not None:
-            module.fail_json(msg="Invalid input -> {}".format(system['primary_method']))
-
-        if system['primary_method'] == system['backup_method1'] or system['primary_method'] == system['backup_method2'] or system['backup_method1'] == system['backup_method2']:
-            if system['backup_method1'] is not None and system['backup_method2'] is not None:
-                module.fail_json(msg="Incomplete command.")
         command.append(cmd)
 
     if enable_console is not None:
@@ -251,43 +221,36 @@ def build_command(module, commands=None, dot1x=None, exec_=None, mac_auth=None, 
 
     return command
 
-def validate_previlege_level(module, commands):
-    """
-    This function is used to validate whether the previlege_level values if given is as expected.
-    """
-    if commands and (commands['privilege_level']) not in [0,4,5]:
-        module.fail_json(msg="Invalid command level {}".format(commands['privilege_level']))
-
 def main():
     """entry point for module execution
     """
     commands_spec = dict(
-        privilege_level=dict(type='int'),
-        primary_method=dict(type='str'),
-        backup_method1=dict(type='str'),
-        backup_method2=dict(type='str'),
+        privilege_level=dict(type='int', choices=[0,4,5]),
+        primary_method=dict(type='str', choices=['radius','tacacs+','none']),
+        backup_method1=dict(type='str', choices=['radius','tacacs+','none']),
+        backup_method2=dict(type='str', choices=['none']),
         state=dict(type='str', default='present', choices=['present', 'absent'])
     )
     dot1x_spec = dict(
-        primary_method=dict(type='str'),
-        backup_method1=dict(type='str'),
+        primary_method=dict(type='str', choices=['radius','none']),
+        backup_method1=dict(type='str', choices=['none']),
         state=dict(type='str', default='present', choices=['present', 'absent'])
     )
     exec_spec = dict(
-        primary_method=dict(type='str'),
-        backup_method1=dict(type='str'),
-        backup_method2=dict(type='str'),
+        primary_method=dict(type='str', choices=['radius','tacacs+','none']),
+        backup_method1=dict(type='str', choices=['radius','tacacs+','none']),
+        backup_method2=dict(type='str', choices=['none']),
         state=dict(type='str', default='present', choices=['present', 'absent'])
     )
     mac_auth_spec = dict(
-        primary_method=dict(type='str'),
-        backup_method1=dict(type='str'),
+        primary_method=dict(type='str', choices=['radius','none']),
+        backup_method1=dict(type='str', choices=['none']),
         state=dict(type='str', default='present', choices=['present', 'absent'])
     )
     system_spec = dict(
-        primary_method=dict(type='str'),
-        backup_method1=dict(type='str'),
-        backup_method2=dict(type='str'),
+        primary_method=dict(type='str', choices=['radius','tacacs+','none']),
+        backup_method1=dict(type='str', choices=['radius','tacacs+','none']),
+        backup_method2=dict(type='str', choices=['none']),
         state=dict(type='str', default='present', choices=['present', 'absent'])
     )
     enable_spec = dict(
@@ -300,7 +263,6 @@ def main():
         mac_auth = dict(type='dict', options=mac_auth_spec),
         system = dict(type='dict', options=system_spec),
         enable_console = dict(type='dict', options=enable_spec),
-        #check_running_config = dict(default=False, type='bool', fallback=(env_fallback, ['ANSIBLE_CHECK_ICX_RUNNING_CONFIG']))
     )
 
     required_one_of = [['commands', 'dot1x', 'exec_', 'mac_auth', 'system']]
@@ -320,14 +282,13 @@ def main():
     if warnings:
         result['warnings'] = warnings
 
-    validate_previlege_level(module, commands)
     commands = [build_command(module, commands, dot1x, exec_, mac_auth, system, enable_console )]
     results['commands'] = commands
 
     if commands:
         if not module.check_mode:
-            for i in results['commands']:
-                response = load_config(module, i)
+            for cmd in results['commands']:
+                response = load_config(module, cmd)
         results['changed'] = True
 
     module.exit_json(**results)

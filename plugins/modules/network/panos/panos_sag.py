29dfff491f31c7adafc2a0d804ca4889f20ac4d4
Deric Crago <deric.crago@redhat.com>
2021-03-16 14:08:51 -0400

    sanity no-log-needed fix

--- plugins/modules/network/panos/panos_sag.py
+++ plugins/modules/network/panos/panos_sag.py
@@ -199,7 +199,7 @@ def delete_address_group(device, dev_group, obj_name):
 def main():
     argument_spec = dict(
         ip_address=dict(required=True),
-        password=dict(required=True),
+        password=dict(required=True, no_log=True),
         username=dict(default='admin'),
         api_key=dict(no_log=True),
         sag_match_filter=dict(type='list', required=False),

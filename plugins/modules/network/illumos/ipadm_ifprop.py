6b687505d3c5199200324a8223ff8b6d7d114c9c
Deric Crago <deric.crago@redhat.com>
2021-10-15 13:49:53 -1000

    Update CI matrix (#312) (#318)
    Co-authored-by: Andrew Klychkov <aklychko@redhat.com>

--- plugins/modules/network/illumos/ipadm_ifprop.py
+++ plugins/modules/network/illumos/ipadm_ifprop.py
@@ -119,7 +119,7 @@ class IfProp(object):
         cmd.append(self.protocol)
         cmd.append(self.interface)
 
-        (rc, _, _) = self.module.run_command(cmd)
+        (rc, dummy, dummy) = self.module.run_command(cmd)
 
         if rc == 0:
             return True
@@ -143,7 +143,7 @@ class IfProp(object):
         cmd.append(self.protocol)
         cmd.append(self.interface)
 
-        (rc, out, _) = self.module.run_command(cmd)
+        (rc, out, dummy) = self.module.run_command(cmd)
 
         out = out.rstrip()
         (value, default) = out.split(':')
@@ -166,7 +166,7 @@ class IfProp(object):
         cmd.append(self.protocol)
         cmd.append(self.interface)
 
-        (rc, out, _) = self.module.run_command(cmd)
+        (rc, out, dummy) = self.module.run_command(cmd)
 
         out = out.rstrip()
 

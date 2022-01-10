8ab233db01ecafb6162d99d4dbe695df688c1737
kb5879gm <97469738+kb5879gm@users.noreply.github.com>
2022-01-10 12:19:29 -0600

    Update eric_eccli.py
    Correction to work with the IPOS v21

--- plugins/terminal/eric_eccli.py
+++ plugins/terminal/eric_eccli.py
@@ -53,7 +53,7 @@ class TerminalModule(TerminalBase):
     def on_open_shell(self):
 
         try:
-            for cmd in (b'screen-length 0', b'screen-width 512'):
+            for cmd in (b'terminal length 0', b'terminal width 512'):
                 self._exec_cli_command(cmd)
         except AnsibleConnectionFailure:
             raise AnsibleConnectionFailure('unable to set terminal parameters')

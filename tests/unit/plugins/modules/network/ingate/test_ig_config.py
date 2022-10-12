bfe2030f3b682715aca18360d8b8ed09df580120
Felix Fontein <felix@fontein.de>
2022-10-12 09:11:01 +0200

    Make sure string replacement works when automatically un-flatmapping.

--- tests/unit/plugins/modules/network/ingate/test_ig_config.py
+++ tests/unit/plugins/modules/network/ingate/test_ig_config.py
@@ -35,9 +35,8 @@ class TestConfigModule(TestIngateModule):
     def setUp(self):
         super(TestConfigModule, self).setUp()
 
-        self.mock_make_request = patch('ansible_collections.community.network.'
-                                       'plugins.modules.network.ingate.'
-                                       'ig_config.make_request')
+        self.mock_make_request = patch(
+            'ansible_collections.community.network.plugins.modules.network.ingate.ig_config.make_request')
         self.make_request = self.mock_make_request.start()
         # ATM the Ingate Python SDK is not needed in this unit test.
         self.module.HAS_INGATESDK = True

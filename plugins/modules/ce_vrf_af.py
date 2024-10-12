b238585a8da2d3f881b9597cb4789c1ca518535a
Mario Lenz <m@riolenz.de>
2024-10-12 14:27:54 +0200

    Update documentation

--- plugins/modules/ce_vrf_af.py
+++ plugins/modules/ce_vrf_af.py
@@ -21,6 +21,10 @@ __metaclass__ = type
 DOCUMENTATION = '''
 ---
 module: ce_vrf_af
+deprecated:
+  removed_in: 6.0.0
+  why: This collection and all content in it is unmaintained and deprecated.
+  alternatives: Unknown.
 short_description: Manages VPN instance address family on HUAWEI CloudEngine switches.
 description:
     - Manages VPN instance address family of HUAWEI CloudEngine switches.
